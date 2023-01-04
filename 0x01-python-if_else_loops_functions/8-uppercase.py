031585b347132ea4ecf9b16adb4f104d013e0a26
Darius Daniel <dariusdaniel771@gmail.com>
2023-01-04 01:45:47 +0100

    Fixed bug in file

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,15 +5,13 @@ def uppercase(str):
     upper_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     end_str, i = '', 0
 
-    if ch == '':
-        print("{}".format('\n'))
-    for ch in str:
-        if i == len(str) - 1:
-            end_str = '\n'
-
-        if ch in lower_chars:
-            print("{}".format(upper_chars[lower_chars.find(ch)]), end=end_str)
-        else:
+    if str != '':
+        for ch in str:
+            if i == len(str) - 1:
+                end_str = '\n'
+            if ch in lower_chars:
+                ch = upper_chars[lower_chars.find(ch)]
             print("{}".format(ch), end=end_str)
-
-        i += 1
+            i += 1
+    else:
+        print("{}".format('\n'))
