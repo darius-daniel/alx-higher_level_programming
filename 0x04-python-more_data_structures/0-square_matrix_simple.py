74cb22e5061e08042db9dd1c4d3497bbf768a72d
Darius Daniel <dariusdaniel771@gmail.com>
2023-01-11 20:14:29 +0100

    Used lambda functions in 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-def sq(x):
-    return (x ** 2)
-
-
 def square_matrix_simple(matrix=[]):
     if matrix:
         new_matrix = []
         for row in matrix:
-            new_matrix.append([sq(num) for num in row])
+            new_matrix.append(list(map(lambda x: x ** 2, row)))
         return new_matrix
