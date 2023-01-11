218ba954d78a58e70fe40f0bb3daa4fcf0291140
Darius Daniel <dariusdaniel771@gmail.com>
2023-01-11 12:33:22 +0100

    Fixed code style issues in 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+def sq(x):
+    return (x ** 2)
+
+
 def square_matrix_simple(matrix=[]):
     if matrix:
         new_matrix = []
-        sq = lambda x: x ** 2
         for row in matrix:
             new_matrix.append([sq(num) for num in row])
         return new_matrix
