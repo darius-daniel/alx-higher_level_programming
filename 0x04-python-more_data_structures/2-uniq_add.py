2389e5d9fabf5a1cc3cfafebd751f50d6f292947
Darius Daniel <dariusdaniel771@gmail.com>
2023-01-11 13:24:50 +0100

    Task 2 done!!

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    if my_list:
+        sum = 0
+        for num in my_list:
+            if my_list.count(num) == 1:
+                sum += num
+        return sum
