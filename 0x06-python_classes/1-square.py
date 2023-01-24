a94cdd580c189cfb397b7ffb81eff01d6af926c2
Darius Daniel <dariusdaniel771@gmail.com>
2023-01-24 23:24:55 +0100

    New directory first commit

---
+++ 0x06-python_classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square
+    """
+    def __init__(self, size):
+        """ Initializes the class
+
+        Args:
+            size: size of the squar
+        """
+        self.__size = size
---
+++ 0x06-python_classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square
+    """
+    def __init__(self, size=0):
+        """ Initializes the class
+
+        Args:
+          size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
