5f1a6f68b43202120508e99163b55d8010b96105
Darius Daniel <dariusdaniel771@gmail.com>
2023-01-25 00:22:50 +0100

    committing

---
+++ 0x06-python_classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square
+    """
+    def __init__(self, size=0):
+        """ Initializes the class
+
+        Args:
+          size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Computes the current square area
+
+        Args:
+            self: the object
+
+        Returns:
+            area of the square
+        """
+        return self.__size ** 2
---
+++ 0x06-python_classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square
+    """
+    def __init__(self, size=0):
+        """ Initializes the class
+
+        Args:
+          size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Computes the current square area
+
+        Args:
+            self: the object
+
+        Returns:
+            area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Retrieves the value of the square instance
+
+        Args:
+            self: instance of the class
+
+        Returns:
+            the area of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Retrieves the value of the size of the square instance
+
+        Args:
+            self: the instance of the class
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
