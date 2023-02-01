71a8f484c38292b4a194760177a14861d96ad756
Darius Daniel <dariusdaniel771@gmail.com>
2023-02-02 00:16:45 +0100

    Added docstring to function in 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,6 +11,12 @@ There should be no space at the beginning or at the end of each printed line
 
 
 def text_indentation(text):
+    """A function that prints a text with 2 new lines after each of the
+    following characters: '.', '?', ':'
+
+    Args:
+        text: a string
+    """
     if type(text) != str:
         raise TypeError("text must be a string")
 
