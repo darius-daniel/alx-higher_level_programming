d6af2357570f9c676d23b637c57ca427c4575b45
Darius Daniel <dariusdaniel771@gmail.com>
2023-02-08 03:45:25 +0100

    Added docstring to 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+"""Contains a function that writes an Object to a text file using a JSON
+representation
+
+Prototype: def save_to_json_file(my_obj, filename):
+    my_obj: a python Object
+    filename: a string containing the name and/or path to be written to.
+"""
 from json import dump
+
+
 def save_to_json_file(my_obj, filename):
+    """A function that writes an Object to a text file using a JSON
+    representation
+
+    Args:
+        my_obj: a python object
+        filename: a string containing the name and/or path of the file to be
+                  written to
+    """
     if filename:
         with open(filename, 'w', encoding="utf-8") as file:
             dump(my_obj, file)
