6d30b56e63fb951c53b07547244e0b6469a5b84f
Darius Daniel <dariusdaniel771@gmail.com>
2023-02-08 03:37:08 +0100

    put import statement after module docstring in 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import json
-
 """Contains a function that returns the JSON representation of an object
 
 Prototype: def to_json_string(my_obj):
     my_obj: a python object
 """
+import json
 
 
 def to_json_string(my_obj):
