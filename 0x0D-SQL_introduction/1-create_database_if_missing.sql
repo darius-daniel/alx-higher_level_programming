2bb85f77aadebd2218c22dbb6ef007e45e81d42f
Darius Daniel <dariusdaniel771@gmail.com>
2023-03-15 02:56:36 +0100

    Removed excess comments lines

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,3 @@
 -- A script that creates a database in the MySQL server if it doesn't exist
 -- the CREATE DATABASE query creates the database. The IF NOT EXISTS is
--- optional and specifies that the new database should be created only if it
--- does not already exist
 CREATE DATABASE IF NOT EXISTS htbn_0c_0;
