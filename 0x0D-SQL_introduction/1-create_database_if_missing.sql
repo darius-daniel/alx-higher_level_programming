d841ff4f288457cb0ced7a6f6378ca929ab1e400
Darius Daniel <dariusdaniel771@gmail.com>
2023-03-15 03:05:18 +0100

    specified the name of the database to be deleted in 2* and fixed typo in database name in 1*

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- A script that creates a database in the MySQL server if it doesn't exist
 -- The CREATE DATABASE query creates the database. The IF NOT EXISTS is
-CREATE DATABASE IF NOT EXISTS htbn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- A script to delete the specified database hbtn_0c_0
 -- This Query deletes the named database if it exists
-DROP DATABASE [IF EXISTS] database_name;
+DROP DATABASE [IF EXISTS] hbtn_0c_0;
