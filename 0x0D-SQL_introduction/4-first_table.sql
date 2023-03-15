60ecbd96da64692362f91e73f04d595e51a81512
Darius Daniel <dariusdaniel771@gmail.com>
2023-03-15 03:17:18 +0100

    Task 4 done

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- This script creates a table @first_table in the current database server
+-- This query creates a table @first_table
+CREATE TABLE IF NOT EXISTS first_table(
+  id INT,
+  name VARCHAR(256)
+  );
