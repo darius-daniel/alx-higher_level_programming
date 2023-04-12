825c789d57759b1d6666e5a43497159a7cbfe514
Darius Daniel <dariusdaniel771@gmail.com>
2023-04-12 05:01:52 +0100

    Added 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+if (!process.argv[2] || !parseInt(process.argv[2])) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 1; i <= process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+}
