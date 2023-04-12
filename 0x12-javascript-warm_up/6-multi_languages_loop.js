0c74d47b67a61809f448d89c8b248e6fa180b0fe
Darius Daniel <dariusdaniel771@gmail.com>
2023-04-12 04:51:57 +0100

    Added 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+languages = ['C', 'Python', 'JavaScript'];
+adjectives = ['fun', 'cool', 'amazing'];
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i] + ' is ' + adjectives[i]);
+}
