11af92cb8ce1981952cd89cd079a50c2dc54479e
Darius Daniel <dariusdaniel771@gmail.com>
2023-04-14 00:29:12 +0100

    Added 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const ProtoSquare = require('./5-square');
+class Square extends ProtoSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += c;
+      }
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Square;
