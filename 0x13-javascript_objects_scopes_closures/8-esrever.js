03f7ee051ced754c8923aa4147fae99547873bee
Darius Daniel <dariusdaniel771@gmail.com>
2023-04-14 00:53:30 +0100

    Finally done with task

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const reversedList = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    reversedList.push(list[i]);
+  if (list.length > 0) {
+    const reversedList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+      reversedList.push(list[i]);
+    }
+    return reversedList;
   }
-  console.log(reversedList);
 };
