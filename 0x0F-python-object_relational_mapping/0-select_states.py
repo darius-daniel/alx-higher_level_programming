9dca07ad59cbadd0e315ce281fb2414bcd51aefe
Darius Daniel <dariusdaniel771@gmail.com>
2023-04-17 14:03:23 +0100

    Fixed code to not run when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,10 +7,10 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(port=3306, user=sys.argv[1], passwd=sys.argv[2],
-                     db=sys.argv[3])
-
 if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
+                         port=3306)
+
     new_cursor = db.cursor()
     new_cursor.execute('SELECT * FROM states;')
     states = new_cursor.fetchall()
