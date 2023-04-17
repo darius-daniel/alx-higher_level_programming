687c5b66f36f26b5578255d90423fba03a27ed16
Darius Daniel <dariusdaniel771@gmail.com>
2023-04-17 13:30:29 +0100

    Formatted code in 0-select_states.py to comply to pep8 formatting guidelines

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ A script that lists all states from the database hbtn_0e_0_usa
 """
+
+
 import MySQLdb
 import sys
 
@@ -8,9 +10,9 @@ db = MySQLdb.connect(port=3306, user=sys.argv[1], passwd=sys.argv[2],
                      db=sys.argv[3])
 
 if __name__ == '__main__':
-    current = db.cursor()
-    current.execute('SELECT * FROM states;')
-    states = current.fetchall()
+    new_cursor = db.cursor()
+    new_cursor.execute('SELECT * FROM states;')
+    states = new_cursor.fetchall()
 
     for state in states:
         print(state)
