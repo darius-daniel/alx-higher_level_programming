82880a20ce632f01989dd6f9c0136fd8c2478b3f
Darius Daniel <dariusdaniel771@gmail.com>
2023-04-18 01:04:15 +0100

    Added many more files to the directory

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-List all cities from a database
+List all cities of a state
 """
 import sys
 import MySQLdb
@@ -11,8 +11,8 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id;")
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
 
-    for state in states:
-        print(state)
+    print(", ".join([state[1] for state in states]))
