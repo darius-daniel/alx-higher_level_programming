be39f8614305b5e60cd32314e4613c53c55e6499
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-13 10:16:31 +0100

    Deleted 2 directories because I deferred the program for a month

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-if (!process.argv[2] || !parseInt(process.argv[2])) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + parseInt(process.argv[2]));
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const languages = ['C', 'Python', 'JavaScript'];
-const adjectives = ['fun', 'cool', 'amazing'];
-
-for (let i = 0; i < languages.length; i++) {
-  console.log(languages[i] + ' is ' + adjectives[i]);
-}
