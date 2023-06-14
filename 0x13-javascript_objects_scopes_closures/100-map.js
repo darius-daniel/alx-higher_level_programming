66888343774d4ac8d53229e9f5c46a8850d3e9f4
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-14 13:55:01 +0100

    Used map() instead of a for loop

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
 const list = require('./100-data').list;
-const newList = [];
-
-for (let i = 0; i < list.length; i++) {
-  newList.push(i * list[i]);
-}
+const newList = list.map((x, i) => x * i);
 
 console.log(list);
 console.log(newList);
