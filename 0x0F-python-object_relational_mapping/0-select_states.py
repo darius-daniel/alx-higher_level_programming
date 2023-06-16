5f3e8771427badc97b65752a13d669e1b59034dc
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-16 15:08:00 +0100

    Added docstring to script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""
+A script that lists all states from a database (hbtn_0e_0_usa)
+"""
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(
-    host='localhost:3306',
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3])
-cur = db.cursor()
-cur.execute('SELECT states FROM htbn_0e_0_usa ORDER BY state_id')
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM htbn_0e_0_usa.states ORDER BY id ASC")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
