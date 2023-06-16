e349eddefca11ae90cbafabc2a3050dcede82dbe
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-16 13:49:15 +0100

    Adding python script for task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,5 +2,10 @@
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(host='localhost:3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-db.curs
+db = MySQLdb.connect(
+    host='localhost:3306',
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3])
+cur = db.cursor()
+cur.execute('SELECT states FROM htbn_0e_0_usa ORDER BY state_id')
