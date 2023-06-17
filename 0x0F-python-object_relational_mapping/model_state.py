561b5996ec135b5ed51b0290440297dea65ab7be
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-17 14:58:04 +0100

    Abother trial to get checker to pass

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,10 +2,10 @@
 """
 A script that contains the class definition of a State and an instance
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+Base = declarative_base(metadata=MetaData())
 
 
 class State(Base):
