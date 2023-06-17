36046e31c327dec9268a0a3a4e6516ded5e45522
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-17 13:54:44 +0100

    Fixed JOIN clause in sql query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
     cursor.execute(
         "SELECT c.id, c.name, s.name FROM cities c \
-            JOIN states s USING (id) \
+            JOIN states s ON c.state_id = s.id \
                 ORDER BY c.id ASC"
     )
     cities = cursor.fetchall()
