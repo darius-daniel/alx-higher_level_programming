c6d0ceb3e0216b8b3ab9c3a06f2fe58dce7f817f
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-17 15:00:53 +0100

    Added docstring to file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,10 +5,13 @@ A script that contains the class definition of a State and an instance
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base(metadata=MetaData())
+Base = declarative_base()
 
 
 class State(Base):
+    """
+    A class definition of a State
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
