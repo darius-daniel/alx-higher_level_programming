9ac620e9a0d3e871557087d1a090ec3ee558d5b3
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-17 16:21:37 +0100

    added the call to all() for the session query on line 22

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%\a%'))
+    states = session.query(State).filter(State.name.like('%\a%')).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
