f6c18f01042a90a75f14d4f1162c6199aa773526
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-18 15:32:35 +0100

    Used session.filter() to filter states with 'a' in their names instead of an if statement

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,10 +18,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).all()
-
+    states = session.query(State).filter(State.name.like("%a%")).all()
     for state in states:
-        if 'a' in state.name:
             session.delete(state)
 
     session.commit()
