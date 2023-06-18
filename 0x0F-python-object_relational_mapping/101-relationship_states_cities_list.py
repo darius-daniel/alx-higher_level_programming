ae201623b7d07d6b6eece4f551e5ca6d9ee3cab4
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-19 00:23:35 +0100

    Import from relationship_state.py and relationship_city.py instead of from model_state.py and model_city.py respectively

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,8 +4,8 @@ A script that lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa
 """
 import sys
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
