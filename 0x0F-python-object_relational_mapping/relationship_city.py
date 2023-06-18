a499d01839c0bb20a3f663758a86728c1f218c7c
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-18 23:03:40 +0100

    Adding solution scripts for tasks 14 and 15

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script containing the class definition of a City
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    A class definition of a City
+    """
+    __tablename__ = 'cities'
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True,
+        unique=True
+    )
+    name = Column(String(128), nullable=False)
+    state_id = Column(
+        Integer,
+        ForeignKey("states.id"),
+        nullable=False
+    )
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A script that contains the class definition of a State and an instance
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    A class definition of a State
+    """
+    __tablename__ = 'states'
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True,
+        unique=True
+    )
+    name = Column(String(128), nullable=False)
+    cities = 
