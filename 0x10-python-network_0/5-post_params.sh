54b4269f26d03d3252112419243cd69607b45aa1
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-30 14:01:02 +0100

    Please work

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that displays the body of the response to a POST request
-curl -s -d "email: test@gmail.com&subject: I will always be here for PLD" -X POST "$1"
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
