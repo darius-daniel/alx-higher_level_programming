e5a31c054d409708e5dc04c684789eba935a43bd
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-30 12:29:58 +0100

    Used correct shebang command

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # A script displays the size of the body of the reponse in bytes
 curl -s -w"%{size_download}\n" -o /dev/null "$1"
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # A script to display the body of the response
 curl -s 
