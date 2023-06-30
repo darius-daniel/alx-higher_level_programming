3899dc150e1560ef43ec6f44568209d4e0a44d20
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-30 12:22:23 +0100

    Another trial

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # A script displays the size of the body of the reponse in bytes
-curl -s -w"%{size_download}" -o /dev/null "$1"
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
