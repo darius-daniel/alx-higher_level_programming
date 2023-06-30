007baa3c9b488930391f5959d98174e4a20087c5
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-30 11:44:14 +0100

    Return -o option and added echo command on the last line to move terminal prompt to new line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 # A bash script that makes a request to a URL and displays the size of the body of the reponse in bytes
-curl -s -w"%{size_download}" "$1"
+curl -s -w"%{size_download}" -o /dev/null "$1"
+echo
