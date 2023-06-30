76c30a31cb06a5c8d5e07d158ec6f6140f08a34d
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-30 13:40:59 +0100

    Rearranged curl options

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that displays the body of the response to a POST request
-curl -s -H "email: test@gmail.com-X POST" -H "subject: I will always be here for PLD" "$1"
+curl -s -H "email: test@gmail.com" -H "subject: I will always be here for PLD" -X POST "$1"
