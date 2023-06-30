75912f18459c821e9625614e33667d462a095c49
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-30 11:50:41 +0100

    Used a line escape instead of an empty echo command

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # A bash script that makes a request to a URL and displays the size of the body of the reponse in bytes
-curl -s -w"%{size_download}" -o /dev/null "$1"; echo
+curl -s -w"%{size_download}\n" -o /dev/null "$1"
