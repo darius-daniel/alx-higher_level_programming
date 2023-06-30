2ef5d74dcad0341fd96891b243e78c800a65289d
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-30 12:23:25 +0100

    Reversed change in previous commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # A script displays the size of the body of the reponse in bytes
-curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
+curl -s -w"%{size_download}\n" -o /dev/null "$1"
