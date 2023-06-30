564cf13a864dbd2f7935ef085c2311ab597200ec
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-30 13:45:34 +0100

    using -d instead of -H option in the curl options

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that displays the body of the response to a POST request
-curl -s -H "email: test@gmail.com" -d "subject: I will always be here for PLD" -d POST "$1"
+curl -s -d "email: test@gmail.com" -d "subject: I will always be here for PLD" -d POST "$1"
