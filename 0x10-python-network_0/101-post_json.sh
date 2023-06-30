f648a55abd087c8d4927a126d811044da27c1a78
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-30 15:28:00 +0100

    Fixed more mistakes in command

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that sends a JSON file as a POST request and displays the body of the response
-curl -s -d "@$2" -X POST -H "Content-Type=application/json" "$1"
+curl -s -d "@$2" -X POST -H "Content-Type: application/json" "$1"
