b713104e096b24284cf9b8f3cfcd91a74d407518
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-02 17:05:59 +0100

    Task  8 done

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that takes in a letter and sends a POST request to a URL with the
+letter as a parameter
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        letter = ''
+    else:
+        letter = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data={'q': letter})
+    try:
+        content = dict(r.text)
+    except Exception as e:
+        print('Not a valid JSON')
+    else:
+        if len(content.keys()) == 0:
+            print("No result")
+        else:
+            print("{} {}".format(content['id'], content['name']))
