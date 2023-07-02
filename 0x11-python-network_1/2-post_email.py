fc71ec7410d83f602fe6aa9689c60ecec74153b0
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-02 15:39:17 +0100

    Removed whitespace from before semicolon in dictionary on line 11

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ import sys
 
 
 if __name__ == "__main__":
-    value = {'email' : sys.argv[2]}
+    value = {'email': sys.argv[2]}
     data = parse.urlencode(value).encode("ascii")
     req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
