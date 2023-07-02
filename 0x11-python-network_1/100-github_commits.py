69168293cb1492e15a885955f0ff4ba083fc2fb4
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-03 00:32:10 +0100

    Advanced task done

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,5 +7,15 @@ import sys
 
 
 if __name__ == '__main__':
-    url = "https://api.github.com/users/{}/hovercard?subject_type=repository".format(sys.argv[1])
-    r = requests.get(url, headers={})
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+    r = requests.get(url)
+    commits = r.json()
+
+    i = 0
+    for commit in commits:
+        if i == 10:
+            break
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
+        i += 1
