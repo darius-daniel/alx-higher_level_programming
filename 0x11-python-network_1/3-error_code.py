19ede8f429ab7deb121eba2bf03c5ed8e16911ee
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-02 16:13:25 +0100

    Made indentation in file to multiples of four

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,8 +9,8 @@ from sys import argv
 
 if __name__ == '__main__':
     try:
-      with request.urlopen(argv[1]) as response:
-        body_content = response.read()
-        print(body_content.decode('utf-8'))
+        with request.urlopen(argv[1]) as response:
+            body_content = response.read()
+            print(body_content.decode('utf-8'))
     except error.HTTPError as e:
-      print("Error code: {}".format(e.code))
+        print("Error code: {}".format(e.code))
