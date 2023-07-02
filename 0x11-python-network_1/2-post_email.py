b12ae83b444c7c018db1a2c24190c390bed5ba42
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-02 15:37:45 +0100

    Adding file for task 2 - POST an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A script that sends a POST request to a URL passed as a cmd line argument
+with an email (also passed as an argument from the command line) as a
+parameter
+"""
+from urllib import request, parse
+import sys
+
+
+if __name__ == "__main__":
+    value = {'email' : sys.argv[2]}
+    data = parse.urlencode(value).encode("ascii")
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode("utf-8"))
