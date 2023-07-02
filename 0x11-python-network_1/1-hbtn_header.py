95f5eb651c65430d1f575780e5222e0762aed54f
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-02 15:14:25 +0100

    Refactored code to not execute when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,6 +5,7 @@ the X-Request-Id variable found in the header of the response
 import urllib.request
 import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    print(response.headers.get("X-Request-Id"))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get("X-Request-Id"))
