3a18b34d3312eaf8f76c343810b784660e1705a3
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-25 16:14:31 +0100

    Modified code in 2 files for advanced tasks

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,7 +2,6 @@
 const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-console.log(url);
 
 request(url, (error, response, body) => {
   if (error) {
