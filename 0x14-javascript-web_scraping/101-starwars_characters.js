bd8faa73bc53b25dc5308134bf0eb9455fc0363b
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-25 15:42:02 +0100

    Formatted script to semistandard

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,17 +5,17 @@ const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 console.log(url);
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        for (const characterURL of JSON.parse(body).characters) {
-            request(characterURL, (err, res, b) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    console.log(JSON.parse(b).name);
-                }
-            });
+  if (error) {
+    console.error(error);
+  } else {
+    for (const characterURL of JSON.parse(body).characters) {
+      request(characterURL, (err, res, b) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(JSON.parse(b).name);
         }
+      });
     }
+  }
 });
